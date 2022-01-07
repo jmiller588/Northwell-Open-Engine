4bbbd15ffd73f05aa9f25c3333f4cef907933f02
jmiller588 <jmiller588@users.noreply.github.com>
2022-01-07 16:33:57 -0600

    Update specialPaste.py

--- Utilities/specialPaste.py
+++ Utilities/specialPaste.py
@@ -1,12 +1,20 @@
 import pyperclip
+import os
 
 copiedText = pyperclip.paste()
 splitText = copiedText.splitlines()
+splitText = [x.strip(' ') for x in splitText]
 
-print(splitText)
+listQuestion = print("""How do you want to separate your list? 
+1. Commas
+2. Quotes
+""")
+question = input()
 
-joinedString = "\n,".join(splitText)
-
-print(joinedString)
-
+if question == "1":
+    newDelim = "\n,"
+    joinedString = newDelim.join(splitText)
+elif question == "2":
+    joinedString = '\n,'.join(f'"{w}"' for w in splitText)
+    
 pyperclip.copy(joinedString)
