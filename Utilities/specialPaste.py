ae42b7f30af6cd14f7cb632de9a17e29109c185f
jmiller588 <jmiller588@users.noreply.github.com>
2022-01-07 16:34:35 -0600

    Update specialPaste.py

--- Utilities/specialPaste.py
+++ Utilities/specialPaste.py
@@ -7,8 +7,7 @@ splitText = [x.strip(' ') for x in splitText]
 
 listQuestion = print("""How do you want to separate your list? 
 1. Commas
-2. Quotes
-""")
+2. Quotes""")
 question = input()
 
 if question == "1":
@@ -16,5 +15,5 @@ if question == "1":
     joinedString = newDelim.join(splitText)
 elif question == "2":
     joinedString = '\n,'.join(f'"{w}"' for w in splitText)
-    
+
 pyperclip.copy(joinedString)
